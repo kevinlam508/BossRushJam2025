cdde2b6225da9f19a92492a283362dfded495cf5
Kevin Lam <kevin.lam72003@gmail.com>
2025-01-24 16:34:06 -0600

    Finish boss 3 vulernerability state

--- SpinningGame/Source/SpinningGame/Private/Boss3Controller.cpp
+++ SpinningGame/Source/SpinningGame/Private/Boss3Controller.cpp
@@ -60,9 +60,62 @@ void ABoss3Controller::Setup(float Duration)
 	RandomizeBoard();
 }
 
+void ABoss3Controller::BeginVulnerability()
+{
+	Super::BeginVulnerability();
+
+	VulnerabilityAnimation.TimePassed = 0;
+	VulnerabilityAnimation.Duration = VulnerabilityRiseFallDuration;
+	VulnerabilityAnimation.StartValue = InvulernablePosition;
+	VulnerabilityAnimation.EndValue = VulernablePosition;
+	FTimerManager& timerManager = GetWorldTimerManager();
+	timerManager.SetTimer(
+		VulnerabilityAnimation.Handle,
+		this,
+		&ABoss3Controller::TickVulnerabilityPositionSwap,
+		1.0 / 60,
+		true
+	);
+}
+
 void ABoss3Controller::EndVulnerability()
 {
 	Super::EndVulnerability();
+	FTimerManager& timerManager = GetWorldTimerManager();
+
+	// Knock player away from board
+	ACharacter* player = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+	player->LaunchCharacter(
+		FVector(0, 0, 10),
+		false,
+		true
+	);
+	FVector towardsPlayer = player->GetActorLocation()
+		- GetPawn()->GetActorLocation();
+	towardsPlayer.Z = 0;
+	towardsPlayer.Normalize();
+	FVector kbForce = towardsPlayer * ExitVulnerabilityKnockback;
+	FTimerDelegate delegate;
+	delegate.BindLambda([&, player, kbForce]()
+	{
+		player->LaunchCharacter(kbForce, true, false);
+	});
+	timerManager.SetTimerForNextTick(delegate);
+
+	// Animate boss back up
+	VulnerabilityAnimation.TimePassed = 0;
+	VulnerabilityAnimation.Duration = VulnerabilityRiseFallDuration;
+	VulnerabilityAnimation.StartValue = VulernablePosition;
+	VulnerabilityAnimation.EndValue = InvulernablePosition;
+	
+	timerManager.SetTimer(
+		VulnerabilityAnimation.Handle,
+		this,
+		&ABoss3Controller::TickVulnerabilityPositionSwap,
+		1.0 / 60,
+		true
+	);
+
 	RandomizeBoard();
 }
 
@@ -88,4 +141,25 @@ void ABoss3Controller::CheckVulerability()
 void ABoss3Controller::OnSetPatternAnimationEnd()
 {
 	RotationColliderEvents->OnToggleRotationColliders.Broadcast(true);
+}
+
+void ABoss3Controller::TickVulnerabilityPositionSwap()
+{
+	FTimerManager& timerManager = GetWorldTimerManager();
+	float elapsed = timerManager.GetTimerElapsed(VulnerabilityAnimation.Handle);
+	VulnerabilityAnimation.TimePassed += elapsed;
+	if (VulnerabilityAnimation.TimePassed > VulnerabilityAnimation.Duration)
+	{
+		GetPawn()->SetActorLocation(VulnerabilityAnimation.EndValue);
+		return;
+	}
+
+	float scalar = VulnerabilityAnimation.TimePassed / VulnerabilityAnimation.Duration;
+	GetPawn()->SetActorLocation(
+		FMath::Lerp(
+			VulnerabilityAnimation.StartValue,
+			VulnerabilityAnimation.EndValue,
+			scalar
+		)
+	);
 }
--- SpinningGame/Source/SpinningGame/Public/Boss3Controller.h
+++ SpinningGame/Source/SpinningGame/Public/Boss3Controller.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "GameFramework/Character.h"
 #include "BaseBossController.h"
 #include "PuzzleBoard.h"
 #include "DamageType_A.h"
@@ -11,7 +12,21 @@
 #include "TimerManager.h"
 #include "RotationColliderEvents.h"
 #include "PuzzleBoardViewComponent.h"
+#include "Kismet/GameplayStatics.h"
 #include "Boss3Controller.generated.h"
+
+
+template<typename T>
+struct AnimationData
+{
+public:
+	T StartValue;
+	T EndValue;
+
+	FTimerHandle Handle;
+	float Duration;
+	float TimePassed;
+};
 
 /**
  * 
@@ -29,6 +44,15 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Puzzle Board")
 	float RandomizePiecesAnimationDuration = 5;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Vulnerability")
+	float VulnerabilityRiseFallDuration = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Vulnerability")
+	FVector VulernablePosition = FVector(1260, 1690, -550);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Vulnerability")
+	FVector InvulernablePosition = FVector(1260, 1690, 90);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Vulnerability")
+	float ExitVulnerabilityKnockback = 3000;
+
 	UFUNCTION(BlueprintCallable)
 	void OnPossess_Implementation(AActor* Actor);
 
@@ -39,6 +63,7 @@ public:
 	void RotateBoardCorner(const FName& CornerName, const TSubclassOf<UDamageType>& DamageType);
 
 	virtual void Setup(float Duration) override;
+	virtual void BeginVulnerability() override;
 	virtual void EndVulnerability() override;
 private:
 
@@ -53,4 +78,8 @@ private:
 
 	UFUNCTION()
 	void OnSetPatternAnimationEnd();
+
+	// Vulnerability animation
+	AnimationData<FVector> VulnerabilityAnimation;
+	void TickVulnerabilityPositionSwap();
 };
